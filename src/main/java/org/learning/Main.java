91919b885b8e29cc42b76df20e7ab5439d917844
Anushree Bhatt <anushreebhatt357@gmail.com>
2024-06-09 08:25:44 -0500

    Creating beans using xml configuration

--- src/main/java/org/learning/Main.java
+++ src/main/java/org/learning/Main.java
@@ -1,30 +1,17 @@
 package org.learning;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Random;
 import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
-        var context = new AnnotationConfigApplicationContext(Config.class);
-        Supplier<Vehicle> v1_supplier = () -> {
-            Vehicle v1 = new Vehicle();
-            v1.setName("Audi");
-            return v1;
-        };
-        Vehicle v2 = new Vehicle();
-        v2.setName("BMW");
-        Supplier<Vehicle> v2_supplier = () -> v2;
+        var context = new ClassPathXmlApplicationContext("beansConfig.xml");
+        Vehicle v = context.getBean("v", Vehicle.class);
+        System.out.println(v.getName());
 
-        Random r = new Random();
-        int randomNumber = r.nextInt(10);
-        if(randomNumber%2==0){
-            context.registerBean("audi_vehicle", Vehicle.class,v1_supplier);
-        }else {
-            context.registerBean("BMW_vehicle", Vehicle.class,v2_supplier);
-        }
-        System.out.println("The random number is :"+randomNumber);
-        System.out.println("Bean created is :"+(context.getBean(Vehicle.class)).getName());
     }
 }
