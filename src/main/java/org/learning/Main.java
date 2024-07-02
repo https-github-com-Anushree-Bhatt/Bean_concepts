dd5f1da3bab70a1eb4fed62f838d71d69f76459f
Anushree Bhatt <anushreebhatt357@gmail.com>
2024-07-01 21:00:36 -0500

    BeanDefinition.SCOPE_SINGLETON

--- src/main/java/org/learning/Main.java
+++ src/main/java/org/learning/Main.java
@@ -4,16 +4,17 @@ package org.learning;
 import org.learning.beans.Person;
 import org.learning.beans.Vehicle;
 import org.learning.configuration.Config;
+import org.learning.service.VehicleService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(Config.class);
-        Person p = context.getBean(Person.class);
-        Vehicle v = p.getVehicle();
-        v.setName("Honda");
-        System.out.println("Vehicle that person owns is:"+v);
-        v.playMusic();
-        v.moveVehicle();
+        VehicleService v1 = context.getBean(VehicleService.class);
+        VehicleService v2 = context.getBean("vehicleService", VehicleService.class);
+        System.out.println("Hashcode of v1:"+(v1.hashCode()));
+        System.out.println("Hashcode of v1:"+(v2.hashCode()));
+        if (v1.equals(v2))
+            System.out.println("VehicleService Bean is singleton scope ");
     }
 }
--- src/main/java/org/learning/service/VehicleService.java
+++ src/main/java/org/learning/service/VehicleService.java
@@ -4,9 +4,12 @@ import org.learning.interfaces.Speaker;
 import org.learning.interfaces.Tyre;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(BeanDefinition.SCOPE_SINGLETON)
 public class VehicleService {
     @Autowired
     @Qualifier(value = "bridgeStone")
